000fc7f4d898e9f1d31ac70dd5dfb0d889bb61f4
Akshat Jangid <akshatjangid16@gmail.com>
2025-02-04 20:01:13 +0530

    furl

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,8 +11,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: ['http://localhost:5173', "https://bighead-fr.vercel.app/"]
+  origin: ['http://localhost:5173', process.env.FRONTEND_URL || '']
 }))
+
 
 
 app.use('/api', UserRouter)
