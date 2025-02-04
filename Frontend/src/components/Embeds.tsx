61deba5d3306bce7a61268f84fcc4e70c0e3b3fd
Akshat Jangid <akshatjangid16@gmail.com>
2025-02-04 19:50:34 +0530

    750

--- Frontend/src/components/Embeds.tsx
+++ Frontend/src/components/Embeds.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function checkLink(link: string | undefined, type: string): string | undefined {
     if (type === "video" && link) {
